98daaea93c898cb81c4d4fafede57419fa68837c
tyler0133 <122943170+tyler0133@users.noreply.github.com>
2023-01-30 23:10:40 -0500

    Update setOperations.java

--- ProbAndStats/src/SetOperations.java
+++ ProbAndStats/src/SetOperations.java
@@ -1,4 +1,50 @@
 
 public class SetOperations {
+   
+       static ArrayList<String> union(ArrayList<String> list1, ArrayList<String> list2)@dd3 {
+       ArrayList<String> unionList = new ArrayList<String>();
+
+      
+       for (String element : list1) {
+           unionList.add(element);
+       }
+
+     
+       for (String element : list2) {
+           if (!unionList.contains(element)) {
+               unionList.add(element);
+           }
+       }
+
+       return unionList;
+   }
+
+   
+       static ArrayList<String> intersect(ArrayList<String> list1, ArrayList<String> list2) {
+       ArrayList<String> intersectList = new ArrayList<String>();
+
+       //add all elements of list1 to intersectList if they exist in list2
+       for (String element : list1) {
+           if (list2.contains(element)) {
+               intersectList.add(element);
+           }
+       }
+
+       return intersectList;
+   }
+
+  
+       static ArrayList<String> complement(ArrayList<String> list1, ArrayList<String> list2) {
+       ArrayList<String> complementList = new ArrayList<String>();
+
+       
+       for (String element : list1) {
+           if (!list2.contains(element)) {
+               complementList.add(element);
+           }
+       }
+
+       return complementList;
+   }
 
 }
